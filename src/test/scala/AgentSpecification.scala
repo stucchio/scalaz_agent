26718f1258fafc710dbf35d842bcb12e4481f8c5
Chris Stucchio <stucchio@gmail.com>
2014-02-23 13:55:22 +0530

    tests for effectfulagent.

--- src/test/scala/AgentSpecification.scala
+++ src/test/scala/AgentSpecification.scala
@@ -3,7 +3,7 @@ package com.chrisstucchio.spire_examples
 import scalaz._
 import Scalaz._
 import scalaz.agent._
-import scalaz.stream._
+import scalaz.stream.{Writer => StreamWriter, _}
 
 import org.scalacheck._
 import org.scalacheck.Arbitrary.arbitrary
@@ -22,6 +22,29 @@ object AgentSpecification extends Properties("AgentSpecification") {
     (agentHistory.toSeq == expectedHistory)
   })
 
+  type LongWriter[X] = Writer[List[Long],X]
+  implicit val LongWriterCatchable = new Catchable[LongWriter] {
+    def attempt[A](f: LongWriter[A]) = f.map(a => \/-(a))
+    def fail[A](err: Throwable) = ???
+  }
 
+  case class CounterLoggingAgent(count: Long = 0, inputsSeen: Long = 0, toWrite: Option[Long] = None) extends EffectfulAgent[Int,CounterLoggingAgent,LongWriter] {
+    def receive(d: Int) = {
+      if (((inputsSeen+1) % 10) == 0) {
+        CounterLoggingAgent(count+d, inputsSeen+1, toWrite = Some(count+d))
+      } else {
+        CounterLoggingAgent(count+d, inputsSeen+1)
+      }
+    }
+    def discard = this.copy(toWrite=None)
+    def effect = toWrite.map( x => List(x).tell ).getOrElse( ().point[LongWriter] ).map(_ => this.discard)
+  }
+
+  property("CounterLoggingAgent works properly") = forAll( (input: List[Boolean]) => {
+    val data = Process.emitSeq(input.map(_ => 1))
+    val agentOutput: LongWriter[Unit] = runAgentForEffect[Nothing,LongWriter,Int,CounterLoggingAgent](data,CounterLoggingAgent()).run
+    val expectedOutput: List[Long] = input.zipWithIndex.filter(x => (x._2+1) % 10 == 0).map(_._2.toLong+1)
+    agentOutput.run._1 == expectedOutput
+  })
 
 }
